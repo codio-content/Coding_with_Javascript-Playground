b0d82a9c5efda179d9b9288ebe9abb857f8ad49c
Ian Jobling <ijobling@codio.com>
2016-06-24 09:47:24 +0000

    initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+// Write any code you like in here
+// 
+// - Run your code from the 'Run current' menu item above
+// - Open the README.md file for more instructions
+// - Don't forget the Lexikon for getting help (Tools->Lexikon menu item)
+
+console.log('Hello world!')
+
